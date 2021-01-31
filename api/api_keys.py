95f7cfca60470fc4605d5690b18f8dfefbf8dc3b
Michael Regpala <mreg96@yahoo.com>
2021-01-31 12:06:04 -0800

    Cool stratify by equatorial lines.  Just wanted to save

--- api/api_keys.py
+++ api/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "39b4c8c3c2107953741b12b2cc76dd00"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCwvIRyo603XKE7JlcV9YkMIkxcKgZT35Q"
